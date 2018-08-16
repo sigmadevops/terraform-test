22f72787866485c5a15ee614981b5cd13010ff9f
anjankumarhn <anjankumar.h@sigmainfo.net>
2018-08-16 17:30:05 +0530

    abcd

--- first/test.tf
+++ first/test.tf
@@ -6,12 +6,13 @@ provider "aws" {
 
 
 terraform {
+	required_version = "~> 0.9.8"
   	backend "s3" {
     bucket = "glowing-sigma-terraform-state"
     key    = "dev/terraform.tfstate"
-    access_key = "${var.aws_access_key}"
-  	secret_key = "${var.aws_secret_key}"
-  	region     = "${var.aws_region}"
+    access_key = "AKIAJLRCG2VXKQW2L44A"
+  	secret_key = "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"
+  	region     = "ap-south-1"
   }
 }
 
