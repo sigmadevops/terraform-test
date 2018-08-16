9fdd0d931ef191bf71c9fdb149aded015cdd5f07
anjankumarhn <anjankumar.h@sigmainfo.net>
2018-08-16 17:39:26 +0530

    abcd

--- first/test.tf
+++ first/test.tf
@@ -5,17 +5,6 @@ provider "aws" {
 }
 
 
-terraform {
-	required_version = "~> 0.9.8"
-  	backend "s3" {
-    bucket = "glowing-sigma-terraform-state"
-    key    = "dev/terraform.tfstate"
-    access_key = "AKIAJLRCG2VXKQW2L44A"
-  	secret_key = "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"
-  	region     = "ap-south-1"
-  }
-}
-
 resource "aws_s3_bucket" "TestBucket" {
   bucket = "jenkins-terraform-test"
   acl    = "private"
